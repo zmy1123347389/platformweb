e9595288e7a0f5272e9fbd5cc5b42bec8a31013a
zmy1123347389 <zmy112334@163.com>
2020-06-22 11:16:52 +0800

    代码提交

---
+++ src/main/java/com/behere/platform/domain/Version.java
@@ -0,0 +1,62 @@
+package com.behere.platform.domain;
+
+/**
+ * @author: Behere
+ */
+public class Version {
+
+    private int id;
+    private int versionNumber;
+    private int forceUpdate;
+    private String updateContent;
+    private int deviceType;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(int versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+
+    public int getForceUpdate() {
+        return forceUpdate;
+    }
+
+    public void setForceUpdate(int forceUpdate) {
+        this.forceUpdate = forceUpdate;
+    }
+
+    public String getUpdateContent() {
+        return updateContent;
+    }
+
+    public void setUpdateContent(String updateContent) {
+        this.updateContent = updateContent;
+    }
+
+    public int getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(int deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/behere/platform/domain/VersionSwitch.java
@@ -0,0 +1,37 @@
+package com.behere.platform.domain;
+
+/**
+ * @author: Behere
+ */
+public class VersionSwitch {
+
+    private int id;
+
+    private int versionNo;
+
+    private int onOff;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVersionNo() {
+        return versionNo;
+    }
+
+    public void setVersionNo(int versionNo) {
+        this.versionNo = versionNo;
+    }
+
+    public int getOnOff() {
+        return onOff;
+    }
+
+    public void setOnOff(int onOff) {
+        this.onOff = onOff;
+    }
+}
